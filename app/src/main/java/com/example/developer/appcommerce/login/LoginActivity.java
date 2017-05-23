968f84abfcd39d0e7e2d3f3969772bb90ecacb44
Kinedu Developer <developer@Gil-MacBook.local>
2017-05-23 16:52:01 -0500

    Login: init Activity

---
+++ app/src/main/java/com/example/developer/appcommerce/login/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.developer.appcommerce.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.developer.appcommerce.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //LoginFragment
+
+        //LoginInteractor
+
+        //LoginPresenter
+    }
+}
